44cdee3331f7a02224da143300f94f7b0508c33a
ValentinChCloud <chavaldranis@gmail.com>
2017-04-18 11:10:08 +0200

    New add

--- script_install_docker.sh
+++ script_install_docker.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-
+#Set the .bashrc
+echo "#Galaxy stuff">>~/.bashrc
+echo "GALAXY_ROOT=$HOME/galaxy">>~/.bashrc
 #Add this line to add mirror for debian and search for docker
 #############################################
 echo "deb http://httpredir.debian.org/debian jessie-backports main">>/etc/apt/sources.list
@@ -25,7 +27,7 @@ mv $path_install_openrefine/GIE/templates $path_galaxy_openrefine
 rm -r $HOME/openrefine-install/OpenRefine-galaxy-ie/GIE
 #Add the path to the interactives environnements if isn't already set
 #############################################
-test_path_interactive_set=$(cat galaxy.ini |grep "interactive_environment_plugins_directory =" |cut -d"=" -f2)
+test_path_interactive_set=$(cat $GALAXY_ROOT/config/galaxy.ini |grep "interactive_environment_plugins_directory =" |cut -d"=" -f2)
 if [ -n "$test_path_ineractive"  ]
 then
 	#echo "The path is already set"
@@ -37,5 +39,6 @@ if [ $# -ne "1" ]
 then
 	echo "bonjour"
 else
-	sed -i "s/\(admin_users = .*\)/\1,$1/" galaxy.ini
+	sed -i "s/\(admin_users = .*\)/\1,$1/" "$GALAXY_ROOT/config/galaxy.ini"
 fi
+
